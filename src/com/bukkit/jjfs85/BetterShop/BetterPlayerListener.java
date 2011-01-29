a0bac98f9cb2e62695da6d44f1f43414baa56913
John Scimone <jjfs85@gmail.com>
2011-01-29 12:37:37 -0800

    added some classes

---
+++ src/com/bukkit/jjfs85/BetterShop/BetterPlayerListener.java
@@ -0,0 +1,14 @@
+package com.bukkit.jjfs85.BetterShop;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerListener;
+
+public class BetterPlayerListener extends PlayerListener {
+		public static BetterShop plugin;
+		public BetterPlayerListener(BetterShop instance) {
+			plugin = instance;
+		}
+		
+		
+}
---
+++ src/com/bukkit/jjfs85/BetterShop/BetterShop.java
@@ -0,0 +1,43 @@
+package com.bukkit.jjfs85.BetterShop;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.bukkit.Server;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginLoader;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
+
+public class BetterShop extends JavaPlugin{
+
+	
+	public BetterShop(PluginLoader pluginLoader, Server instance,
+			PluginDescriptionFile desc, File folder, File plugin,
+			ClassLoader cLoader) {
+		super(pluginLoader, instance, desc, folder, plugin, cLoader);
+	}
+/**
+ *  BetterShop
+ *  Author: jjfs85
+ *  Version: 0.01
+ */
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
