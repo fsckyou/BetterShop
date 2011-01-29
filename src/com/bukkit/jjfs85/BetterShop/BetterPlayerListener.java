b78500bbbf995af22bb1c5d5f40b6bfeeb326a0d
John Scimone <jjfs85@gmail.com>
2011-01-29 15:33:55 -0800

    Made some headway parsing and checking commands. Not working yet. Needs more magic.

--- src/com/bukkit/jjfs85/BetterShop/BetterPlayerListener.java
+++ src/com/bukkit/jjfs85/BetterShop/BetterPlayerListener.java
@@ -5,33 +5,57 @@ import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerListener;
 
 public class BetterPlayerListener extends PlayerListener {
-		public static BetterShop plugin;
-		public BetterPlayerListener(BetterShop instance) {
-			plugin = instance;
+	public static BetterShop plugin;
+
+	public BetterPlayerListener(BetterShop instance) {
+		plugin = instance;
+	}
+
+	@Override
+	public void onPlayerCommand(PlayerChatEvent event) {
+		@SuppressWarnings("unused")
+		int i = 0, a = 0, b = 0, s = 0;
+		// Make the message a string.
+		String[] split = event.getMessage().split(" ");
+		// Get the player that talked.
+		Player player = event.getPlayer();
+		// Accept shop command, parse arguments, check values
+		// TODO EVERYTHING HERE.
+		if (split[0].equalsIgnoreCase("/shop")) {
+			if (split.length > 2) {
+				if ((i = this.stringToInt(split[2])) < 0) {
+					player.sendMessage("Invalid Item.");
+				}
+			}
+			if (split[1].equalsIgnoreCase("list")) {
+				if (split.length == 3)
+					plugin.list(player, i);
+				else
+					plugin.list(player, 1);
+			}
+			if (split[1].equalsIgnoreCase("buy")) {
+				if (split.length == 4)
+					plugin.buy(player, i, a);
+				else
+					plugin.help(player);
+			}
+			if (split[1].equalsIgnoreCase("sell")) {
+				if (split.length == 4)
+					plugin.sell(player, i, a);
+				else
+					plugin.help(player);
+			}
+			event.setCancelled(true);
 		}
-		
-		public void onPlayerCommand(PlayerChatEvent event){
-            //Make the message a string.
-            String[] split = event.getMessage().split(" ");
-            //Get the player that talked.
-            Player player = event.getPlayer();
-            //Accept shop command, parse arguments, check values
-            if (split[0].equalsIgnoreCase("/shop")) {
-            		if (split[1].equalsIgnoreCase("list"))
-            			if (split.length == 3)
-            				plugin.list(player, split[2]);
-            			else
-            				plugin.list(player, 1);
-            		if (split[1].equalsIgnoreCase("buy"))
-            			if (split.length == 4)
-            				plugin.buy(player, split[2], split[3]);
-            		if (split[1].equalsIgnoreCase("sell"))
-            			if (split.length == 4)
-            				plugin.sell(player, split[2], split[3]);
-                    event.setCancelled(true);
-            }
+	}
+
+	private int stringToInt(String s) {
+		int i;
+		try {
+			i = Integer.parseInt(s);
+		} catch (NumberFormatException nfe) {
+			return -1;
 		}
-		private int StringToInt(string s) {
-			
-		}
+		return i;
+	}
 }
--- src/com/bukkit/jjfs85/BetterShop/BetterShop.java
+++ src/com/bukkit/jjfs85/BetterShop/BetterShop.java
@@ -1,49 +1,58 @@
 package com.bukkit.jjfs85.BetterShop;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
+
 import org.bukkit.Server;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.PluginManager;
-import com.nijikokun.bukkit.iConomy.iConomy;
 
-public class BetterShop extends JavaPlugin{
+public class BetterShop extends JavaPlugin {
 
-	
 	public BetterShop(PluginLoader pluginLoader, Server instance,
 			PluginDescriptionFile desc, File folder, File plugin,
 			ClassLoader cLoader) {
 		super(pluginLoader, instance, desc, folder, plugin, cLoader);
 	}
-/**
- *  BetterShop
- *  Author: jjfs85
- *  Version: 0.01
- */
+
+	/**
+	 * BetterShop Author: jjfs85 Version: 0.01
+	 */
 
 	@Override
 	public void onDisable() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onEnable() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public void list(Player player, int page) {
-		
+		// TODO Implement list method
 	}
-	
+
+	public void buy(Player player, int item, int amt) {
+		// TODO Implement buy method
+	}
+
+	public void sell(Player player, int item, int amt) {
+		// TODO Implement sell method
+	}
+
+	public void add(Player player, int item, int amt, int buyamt, int sellamt) {
+		// TODO Implement add method
+	}
+
+	public void remove(Player player, int item, int amt, int buyamt, int sellamt) {
+		// TODO Implement remove method
+	}
+
 	public void help(Player player) {
-		
+		// TODO Implement help method
 	}
 }
